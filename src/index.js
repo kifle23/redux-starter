52bd79c4a7838bf4189ddc2c15b6d183fa496613
kifle23 <68891198+kifle23@users.noreply.github.com>
2023-06-12 21:13:36 +0300

    Setup Cart Slice

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { store } from './store';
 import { Provider } from 'react-redux';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
