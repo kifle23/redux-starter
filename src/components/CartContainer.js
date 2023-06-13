ce4bd5432ec3c7248e82313b200b90a6ba08af27
kifle23 <68891198+kifle23@users.noreply.github.com>
2023-06-13 08:25:09 +0300

    Create reducer clearCart

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -1,14 +1,14 @@
-import React from 'react';
 import CartItem from './CartItem';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { clearCart } from '../features/cart/cartSlice';
 
 const CartContainer = () => {
-  const { cartItems, total, amount } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  const { cartItems, total, amount } = useSelector((store) => store.cart);
 
   if (amount < 1) {
     return (
       <section className="cart">
-        {/* cart header */}
         <header>
           <h2>your bag</h2>
           <h4 className="empty-cart">is currently empty</h4>
@@ -16,30 +16,29 @@ const CartContainer = () => {
       </section>
     );
   }
+
   return (
     <section className="cart">
-      {/* cart header */}
       <header>
         <h2>your bag</h2>
       </header>
-      {/* cart items */}
       <div>
         {cartItems.map((item) => {
           return <CartItem key={item.id} {...item} />;
         })}
       </div>
-      {/* cart footer */}
       <footer>
         <hr />
         <div className="cart-total">
           <h4>
-            total <span>${total}</span>
+            total <span>${total.toFixed(2)}</span>
           </h4>
         </div>
-        <button className="btn clear-btn">clear cart</button>
+        <button className="btn clear-btn" onClick={() => dispatch(clearCart())}>
+          clear cart
+        </button>
       </footer>
     </section>
   );
 };
-
 export default CartContainer;
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -11,8 +11,13 @@ const initialState = {
 const cartSlice = createSlice({
   name: 'cart',
   initialState,
+  reducers: {
+    clearCart: (state) => {
+      state.cartItems = [];
+    },
+  },
 });
 
-console.log(cartSlice);
+export const { clearCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
